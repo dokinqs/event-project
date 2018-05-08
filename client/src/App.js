27b71b02429df30f8f13cf9187f01c3082635567
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-05-08 12:30:20 -0400

    Felipe and Janell worked on Event.js get all

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,35 @@ import Events from './components/Events';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: []
+    }
+  }
+
+  fetchEvents() {
+    fetch('/event')
+      .then(resp => {
+        if (!resp.ok) {
+          throw Error('oops: ', resp.message);
+        }
+
+        return resp.json();
+      }).then(data => this.setState ({
+          events: data.data
+      })).catch(err => console.log(`error: ${err}`))
+  }
+
+  componentDidMount() {
+    this.fetchEvents();
+  }
+
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={Events} />
+        <Route exact path="/event" component={Events} />
       </div>
     );
   }
--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -4,6 +4,12 @@ export default (props) => {
   return (
     <div>
     <h2>Welcome Home!</h2>
+
+    {this.props.event.map(event => (
+          <div key={event.id}>
+            <p>{event.text}
+          </div>
+        ))}
   </div>
   )
 }
