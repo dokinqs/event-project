ced140bc88f7d0a436190cc98b9f71af04b3f5ef
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-05-09 18:52:09 -0400

    added comments to api.js, authRouter.js, tokenService.js

--- services/tokenService.js
+++ services/tokenService.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 const secret = process.env.TOKEN_SECRET || 'supersecuresecret';
 
+// makes a token for the user
+// attaches it to the user
 function makeToken(payload) {
-  console.log('HIIII')
+  // console.log('HIIII')
   return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
@@ -12,6 +14,9 @@ function makeToken(payload) {
   });
 }
 
+// will verify the user by token.
+// If the token is the same it will accept
+// if it is not familiar it will reject the user
 function verify(token) {
   return new Promise((resolve, reject) => {
     jwt.verify(
