d7b552ce2b3f8c821149587b160a1405cc1e639d
Chance Baars <chance@ool-ad020102.dyn.optonline.net>
2018-05-10 14:16:42 -0400

    HOuseKeeping

--- client/src/components/EditEvent.js
+++ client/src/components/EditEvent.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
 export default class EditEvent extends Component {
-  // contructor(props){
-  //   super(props);
-  //   this.state = {
-  //     event: Object.assign({
-  //       event: '',
-  //       text: ''
-  //     }, props.event)
-  //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      event: Object.assign({
+        event: '',
+        text: '',
+        image_url: ''
+      }, props.event)
+    }
+  }
   handleChange(e) {
     const { name, value} = e.target;
     this.setState((prevState, props) => ({
@@ -18,6 +19,8 @@ export default class EditEvent extends Component {
         [name]: value
       }
     }))
+
+    console.log("name: ", name, "event: ", value)
   }
   handleSubmit(e) {
     e.preventDefault();
@@ -26,12 +29,39 @@ export default class EditEvent extends Component {
   }
 
   render() {
-    // const { event, text, id, img_url} = this.state.event
+    const { event, text, id, img_url} = this.state.event
     return (
       <div>
         <h1>EditEvent nr: {this.props.event.id}</h1>
         <p>this is the event: {this.props.event.event}</p>
         <p>this is the event: {this.props.event.text}</p>
+        <form>
+          <label>
+            <h3>Text</h3>
+            <textarea rows='8' colts ='80'
+              name='text'
+              value={text}
+              onChange={this.handleChange.bind(this)}
+            />
+          </label><br/>
+          <label>
+            <h3>Event</h3>
+            <textarea rows='8' colts ='80'
+              name='event'
+              value={event}
+              onChange={this.handleChange.bind(this)}
+            />
+          </label><br/>
+          <label>
+            <h3>image URL</h3>
+            <textarea rows='8' colts ='80'
+              name='image_url'
+              value={image_url}
+              onChange={this.handleChange.bind(this)}
+            />
+          </label><br/>
+          <button type='submit'>CHANGE</button>
+        </form>
 
       </div>
     )
