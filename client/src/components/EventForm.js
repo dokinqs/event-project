aef5e454deea67e51d1572854c02bd2fef666302
Felipe Henao <doneright@Felipes-MBP.home>
2018-05-13 13:40:21 -0400

    toggle between create/edit on form

--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -64,7 +64,7 @@ export default class EventForm extends Component {
 						/>
 					</label><br/>
 
-					<button type='submit'>CHANGE</button>
+					<button type='submit'>{id ? 'edit' : 'create'}</button>
 				</form>
 
 			</div>
