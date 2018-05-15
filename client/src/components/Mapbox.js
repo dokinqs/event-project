18656ab6cd87b95609e70e5eb0852ea9aff92dfa
Felipe Henao <doneright@ool-ad0201bf.dyn.optonline.net>
2018-05-15 11:18:46 -0400

    added some code to test the map tagging feauture of geocode, there. Website doesn's start

--- client/src/components/Mapbox.js
+++ client/src/components/Mapbox.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import mapboxgl from "mapbox-gl";
-// import Geocoder from '@mapbox/react-geocoder';
+import Geocoder from '@mapbox/react-geocoder';
 
 mapboxgl.accessToken = "pk.eyJ1IjoiaGFja3VwIiwiYSI6ImNqaDI3cGtwdTBiemIyd2xpODE5NTB0YWQifQ.9Z2ES7XsDMO-_5GwV43qgw";
 
@@ -26,15 +26,57 @@ class Mapbox extends Component {
 	      zoom: 10
 	    });
 
-	    map.on('move', () => {
-	      const { lng, lat } = map.getCenter();
+			// testing to see if a marker will appear
+			// const marker = new mapboxgl.Marker()
+			// .setLngLat([-21.9270884, 64.1436456])
+			// .addTo(map);
 
-	      this.setState({
-	        lng: lng.toFixed(4),
-	        lat: lat.toFixed(4),
-	        zoom: map.getZoom().toFixed(2)
-	      });
-	    });
+			// map.addControl(geocoder);
+
+			// testing to see if setting a point after geocoder result
+			// const geocoder = new MapboxGeocoder({
+			// 	accessToken: mapboxgl.accessToken
+			// });
+
+			// After the map style has loaded on the page, add
+			// a source layer and default style for a single point
+			// map.on('load', function() {
+			// 	map.addSource('single-point', {
+			// 		"type": "geojson",
+			// 		"data": {
+			// 			"type": "FeatureCollection",
+			// 			"feature": []
+			// 		}
+			// 	});
+			//
+			// map.addLayer({
+			// 	"id": "point",
+			// 	"source": "singple-parent",
+			// 	"type": "circle",
+			// 	"paint": {
+			// 		"circle-radius": 10,
+			// 		"circle-color": "#007cbf"
+			// 	}
+			// });
+
+			// listens for the 'geocoder.input' event that is triggered
+			// when a user makes a selection and ass a symbol that
+			// matches the result.
+
+			// geocoder.on('result', function(ev) {
+			// 	map.getSource('single-point').setData(ev.result.geometry);
+			// });
+	// });
+
+	    // map.on('move', () => {
+	    //   const { lng, lat } = map.getCenter();
+			//
+	    //   this.setState({
+	    //     lng: lng.toFixed(4),
+	    //     lat: lat.toFixed(4),
+	    //     zoom: map.getZoom().toFixed(2)
+	    //   });
+	    // });
 
 
 	  }
